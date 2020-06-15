d9af92c178ec8f46e6b272a1fd9eb7578b2d2605
Syamala Umamaheswaran <shyam@oss.volkswagen.com>
2020-06-15 14:38:46 +0200

    add support for optional path in bind-route-service
    Signed-off-by: Syamala Umamaheswaran <shyam@oss.volkswagen.com>
    
    Co-authored-by: Maic Siemering <maic@oss.volkswagen.com>

--- resource/commands/bind-route-service.sh
+++ resource/commands/bind-route-service.sh
@@ -2,8 +2,9 @@
 domain=$(get_option '.domain')
 service_instance=$(get_option '.service_instance')
 hostname=$(get_option '.hostname')
+path=$(get_option '.path')
 
 logger::info "Executing $(logger::highlight "$command"): $service_instance"
 
 cf::target "$org" "$space"
-cf::bind_route_service "$domain" "$service_instance" "$hostname"
+cf::bind_route_service "$domain" "$service_instance" "$hostname" "$path"
