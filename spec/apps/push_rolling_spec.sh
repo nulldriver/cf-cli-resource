66bdde8dec907ca2c936580e2519c786bbb22a9c
patrickcrocker <patrick.crocker@gmail.com>
2020-10-17 21:10:45 -0500

    add default strategy push before doing rolling strategy

--- spec/apps/push_rolling_spec.sh
+++ spec/apps/push_rolling_spec.sh
@@ -25,6 +25,29 @@ Describe 'apps'
   BeforeAll 'setup'
   AfterAll 'teardown'
 
+  It 'can push an app'
+    push_app() {
+      local fixture=$(load_fixture "static-app")
+      local params=$(
+        %text:expand
+        #|command: push
+        #|org: $org
+        #|space: $space
+        #|app_name: $app_name
+        #|path: $fixture/dist
+        #|memory: 64M
+        #|disk_quota: 64M
+        #|instances: 2
+      )
+      put_with_params "$(yaml_to_json "$params")"
+    }
+    When call push_app
+    The status should be success
+    The output should json '.version | keys == ["timestamp"]'
+    The error should include "Pushing app"
+    Assert cf::is_app_started "$app_name"
+  End
+
   It 'can push an app using rolling strategy'
     push_app() {
       local fixture=$(load_fixture "static-app")
@@ -37,16 +60,16 @@ Describe 'apps'
         #|path: $fixture/dist
         #|memory: 64M
         #|disk_quota: 64M
-        #|strategy: rolling
         #|instances: 2
+        #|strategy: rolling
       )
       put_with_params "$(yaml_to_json "$params")"
     }
     When call push_app
     The status should be success
-    The error should include "#0   running"
-    The error should include "#1   running"
     The output should json '.version | keys == ["timestamp"]'
+    The error should include "Pushing app"
     Assert cf::is_app_started "$app_name"
+    # TODO: how do we test that the strategy actually worked?
   End
 End
