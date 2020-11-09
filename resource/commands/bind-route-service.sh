1bedfa53f1573df7b0bad0516818e2aa69c968e9
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2020-11-09 11:55:38 +0100

    Add support for 'configuration' parameter to bind-route-service.sh

--- resource/commands/bind-route-service.sh
+++ resource/commands/bind-route-service.sh
@@ -3,6 +3,7 @@ domain=$(get_option '.domain')
 service_instance=$(get_option '.service_instance')
 hostname=$(get_option '.hostname')
 path=$(get_option '.path')
+configuration=$(get_option '.configuration')
 
 logger::info "Executing $(logger::highlight "$command"): $service_instance"
 
@@ -11,5 +12,6 @@ cf::target "$org" "$space"
 args=("$domain" "$service_instance")
 [ -n "$hostname" ] && args+=(--hostname "$hostname")
 [ -n "$path" ] && args+=(--path "$path")
+[ -n "$configuration" ] && args+=(-c "configuration")
 
 cf::cf bind-route-service "${args[@]}"
