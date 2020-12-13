db51f432aecbe8b2424c6fafec219ca031bf917f
patrickcrocker <patrick.crocker@gmail.com>
2020-12-13 13:38:37 -0600

    add rollback test

--- spec/apps/zero-downtime-push_spec.sh
+++ spec/apps/zero-downtime-push_spec.sh
@@ -76,4 +76,50 @@ Describe 'apps'
     Assert cf::is_app_started "$app_name"
     Assert not cf::app_exists "$app_name-venerable"
   End
+
+  It 'can rollback a failed push'
+    push_with_insufficient_disk_quota() {
+      local fixture=$(load_fixture "static-app")
+      local params=$(
+        %text:expand
+        #|command: zero-downtime-push
+        #|org: $org
+        #|space: $space
+        #|current_app_name: $app_name
+        #|manifest: $fixture/manifest.yml
+        #|path: $fixture/dist
+        #|vars:
+        #|  app_name: $app_name
+        #|  disk_quota: 1M
+        #|vars_files:
+        #|  - $fixture/vars-memory.yml
+      )
+      put_with_params "$(yaml_to_json "$params")"
+    }
+    When call push_with_insufficient_disk_quota
+    The status should eq $E_ZERO_DOWNTIME_PUSH_FAILED
+    The output should json '.version | keys == ["timestamp"]'
+    The error should include "Renaming app"
+    The error should include "Staging app"
+    The error should include "Error encountered during zero-downtime-push. Rolling back to current app."
+    The error should include "Deleting app"
+    Assert cf::is_app_started "$app_name"
+    Assert not cf::app_exists "$app_name-venerable"
+  End
+
+  It 'can error if manifest not found'
+    push_with_invalid_manifest() {
+      local fixture=$(load_fixture "static-app")
+      local params=$(
+        %text:expand
+        #|command: zero-downtime-push
+        #|manifest: $fixture/does_not_exist.yml
+      )
+      put_with_params "$(yaml_to_json "$params")"
+    }
+    When call push_with_invalid_manifest
+    The status should eq $E_MANIFEST_FILE_NOT_FOUND
+    The output should json '.version | keys == ["timestamp"]'
+    The error should match pattern "*invalid payload (manifest is not a file: */does_not_exist.yml*)"
+  End
 End
